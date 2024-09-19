640be7b07deaf41400b7e615a30e345fea76ca33
Scott Warchal <scott.warchal@gmail.com>
2024-09-19 15:52:00 +0100

    fix colors for napari

--- sparcstools/stitch.py
+++ sparcstools/stitch.py
@@ -518,15 +518,15 @@ def generate_stitched(
         axes = "cyx"
 
         channel_colors = [
-            "#e60049",
-            "#0bb4ff",
-            "#50e991",
-            "#e6d800",
-            "#9b19f5",
-            "#ffa300",
-            "#dc0ab4",
-            "#b3d4ff",
-            "#00bfa0",
+            "e60049",
+            "0bb4ff",
+            "50e991",
+            "e6d800",
+            "9b19f5",
+            "ffa300",
+            "dc0ab4",
+            "b3d4ff",
+            "00bfa0",
         ]
 
         # check if length of colors is enough for all channels in slide otherwise loop through n times
